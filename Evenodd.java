186a3a43e7513b60f3fb92acae275372aa7f4597
Aharva Karale <atharvakarale2604@gmail.com>
2025-01-04 21:49:32 +0530

    Add files via upload

---
+++ Evenodd.java
@@ -0,0 +1,9 @@
+public class Evenodd {
+    public static void main(String[] args) {
+        int n=39;
+        if(n%2==0)
+        System.out.println("Even number");
+        else 
+        System.out.println("Odd Number");
+    }
+}
---
+++ Swap.java
@@ -0,0 +1,12 @@
+public class Swap {
+    public static void main(String[] args) {
+        int a=12,b=3;
+        //
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+    
+}
