b841fd7a78b1724ff08c0809ce205f04e2001f5e
Aharva Karale <atharvakarale2604@gmail.com>
2025-01-04 21:50:28 +0530

    Update Evenodd.java

--- Evenodd.java
+++ Evenodd.java
@@ -1,7 +1,7 @@
 public class Evenodd {
     public static void main(String[] args) {
         int n=39;
-        if(n%2==0)
+        if(n%2==0) // condition for Even number 
         System.out.println("Even number");
         else 
         System.out.println("Odd Number");
